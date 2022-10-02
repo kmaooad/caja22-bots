a1deb197f7de6c521e648a0a3ac9811b9abebaa3
xOooQOFU <oleksandra.shliakhova@ukma.edu.ua>
2022-10-02 18:45:14 +0300

    Added message id request

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -8,6 +8,7 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import org.json.JSONObject;
 
 import java.util.Optional;
 
@@ -35,6 +36,40 @@ public class Function {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name in the request body").build();
         } else {
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
+    }
+    @FunctionName("TelegramWebhookMessageID")
+    public HttpResponseMessage runMessageID(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.POST},
+                authLevel = AuthorizationLevel.FUNCTION)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        final String ObjectName = "message";
+        final String FieldName = "message_id";
+        final String SuccessResponsePrefix = "200 OK: ";
+        final String FailResponse = "BAD REQUEST";
+
+        try
+        {
+            final String RequestBody = request.getBody().orElse(null);
+            if (RequestBody != null)
+            {
+                JSONObject JSONBody = new JSONObject(RequestBody);
+                final int MessageID = JSONBody.getJSONObject(ObjectName).getInt(FieldName);
+                return request.createResponseBuilder(HttpStatus.OK).body(SuccessResponsePrefix + MessageID).build();
+            }
+            else
+            {
+                return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Body is empty").build();
+            }
+        }
+        catch (Exception e)
+        {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(FailResponse).build();
         }
     }
 }
